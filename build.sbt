651afb6f418f3c973688287a2a704b61ea92c43b
linhnt <linhnt411@gmail.com>
2018-12-11 14:31:28 +0700

    mqtt publish demo

--- build.sbt
+++ build.sbt
@@ -4,3 +4,7 @@ name := "scala_example"
 version := "0.1"
 
 scalaVersion := "2.12.7"
+
+libraryDependencies += "org.eclipse.paho" % "mqtt-client" % "0.4.0"
+
+resolvers += "MQTT Repository" at "https://repo.eclipse.org/content/repositories/paho-releases/"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,44 @@
+import org.eclipse.paho.client.mqttv3.{MqttClient, MqttConnectOptions, MqttException, MqttMessage}
+import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("done!")
+    val brokerUrl = "tcp://localhost:1883"
+    val topic = "test"
+    val msg = "Hello world test data"
+
+    var client: MqttClient = null
+
+    // Creating new persistence for mqtt client
+    val persistence = new MqttDefaultFilePersistence("/tmp")
+
+    val options:MqttConnectOptions = new MqttConnectOptions
+    options.setUserName("linhnt")
+    options.setPassword("123456".toCharArray)
+    options.setCleanSession(true)
+    options.setKeepAliveInterval(3600)
+      try {
+        // mqtt client with specific url and client id
+        client = new MqttClient(brokerUrl, MqttClient.generateClientId, persistence)
+
+        client.connect(options)
+
+        val msgTopic = client.getTopic(topic)
+        val message = new MqttMessage(msg.getBytes("utf-8"))
+
+        while (true) {
+          msgTopic.publish(message)
+          println("Publishing Data, Topic : %s, Message : %s".format(msgTopic.getName, message))
+          Thread.sleep(100)
+        }
+      }
+
+      catch {
+        case e: MqttException => println("Exception Caught: " + e)
+      }
+
+      finally {
+        client.disconnect()
+      }
   }
 }
